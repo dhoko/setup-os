2987bcdcf045497bb28b713bbb89512699a0a978
dhoko <dhoko@cyaneus.org>
2015-08-29 17:48:40 +0200

    Create command to setup the env

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # setup-os
 Script to setup my desktop (based on Ubuntu)
+
+## Software
+
+- htop
+- vim 
+- synapse 
+- git 
+- curl 
+- xclip 
+- leafpad 
+- guake 
+- sakura
+- unrar 
+- vlc 
+- clementine
+- Chromium
+
+
+## Non free Software
+
+- Google Chrome
+- Sublime text 3
+- Skype
---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+echo Install Ubuntu
+
+
+# Add PPAs
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+
+sudo apt-get update
+
+sudo apt-get install htop vim synapse sakura git curl xclip leafpad guake unrar vlc clementine chromium-browser
+sudo apt-get install google-chrome-stable sublime-text-installer skype
+
+
+# Import dotfiles
+cd /tmp
+git https://github.com/dhoko/dotfiles.git
+cd dotfiles/
+cp -r .bashrc ~/.bashrc
+cp -r .bashrc.d ~/.bashrc.d
+cp -r .tmux ~/.tmux
+mkdir ~/.fonts
+cp .fonts.conf ~/.fonts.conf
+
+# Install docker
+wget -qO- https://get.docker.com/ | sh
+sudo usermod -aG docker dhoko
+
+# Install node.js
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
+source ~/.bashrc
+
+nvm install stable
+
+echo
+
+echo Now you need to:
+echo - Install Monaco fonts
+echo - Import ssh keys
+echo - Import passwords
+echo - Import images
+echo - Drink a beer
+echo
+echo
+
+echo So long and thanks for all the fish !
