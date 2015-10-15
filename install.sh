bd7d9243b39b44564cb527dcebda115c7e3ee3c5
dhoko <dhoko@users.noreply.github.com>
2015-10-15 14:32:46 +0200

    Add ppa for synapse

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ echo Install Ubuntu
 
 # Add PPAs
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo add-apt-repository ppa:synapse-core/ppa
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
